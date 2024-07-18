869083720664bf7188de45d11027c2dce426c341
Pedro Cursino <81772227+pedro-cursino@users.noreply.github.com>
2024-07-17 21:20:07 -0300

    Update script_clima.js

--- Scripts/script_clima.js
+++ Scripts/script_clima.js
@@ -1,13 +1,18 @@
 import api_key from '../config.js';
 
+// Variáveis
+const api_key = "64ed82577ced7f69cb1687f0ce536131";
+
 const temp_number = document.querySelector('.container-temp');
 
+// Evento de carga da janela
 window.addEventListener('load', () => {
     const lat = -23.0264;
     const long = -45.5553;
     coordResults(lat, long);
 });
 
+// Função para obter dados usando coordenadas
 function coordResults(lat, long) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&lang=pt_br&units=metric&APPID=${api_key}`)
         .then(response => {
@@ -24,6 +29,7 @@ function coordResults(lat, long) {
         });
 }
 
+// Função para exibir os resultados
 function displayResults(weather) {
     temp_number.innerHTML = Math.round(weather.main.temp);
 }
